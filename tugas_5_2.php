9dc8c3810f0add566aff46fbb9dc6aa43d6ad0e2
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-08 10:47:33 +0700

    Add files via upload

---
+++ tugas_5_2.php
@@ -0,0 +1,31 @@
+<?php
+$a = "Ayo";
+$b = "Belajar";
+$c = "Bersama";
+$d = "Niomic";
+
+
+echo "<h1> Operating String </h1>";
+echo "<table border=1>
+	<tr>
+		<td> Input 1 </td>
+		<td> Input 2 </td>
+		<td> hasil </td>
+	
+	</tr>
+	<tr>
+		<td> $a </td>
+		<td> $b </td>
+		<td> $a $b </td>
+		
+	</tr>
+	<tr>
+		<td> $c </td>
+		<td> $d </td>
+		<td> $c $d </td>
+	
+	</tr>
+	
+</table>"
+
+?>
